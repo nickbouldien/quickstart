5557f81ab363e36863111457af145085e5c055f8
nick bouldien <nick.bouldien@gmail.com>
2017-05-14 22:12:36 -0700

    change books-list.component.html/ts to work on data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { BooksListComponent } from './books/books-list/books-list.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent , BooksListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
